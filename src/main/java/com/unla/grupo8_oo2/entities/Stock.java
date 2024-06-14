19472578cf55dd187578f4e82ff084fec337fed4
enzo-lopez <enzofabianlopez96@gmail.com>
2024-06-14 17:32:37 -0300

    agregada la entidad Stock

---
+++ src/main/java/com/unla/grupo8_oo2/entities/Stock.java
@@ -0,0 +1,32 @@
+package com.unla.grupo8_oo2.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class Stock {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@OneToOne
+	@JoinColumn(name="producto_id")
+	private Producto producto;
+	
+	@Column(name="stockActual")
+	private int stockActual;
+	
+	@Column(name="stockCritico")
+	private int descripcion;
+	
+	
+}
