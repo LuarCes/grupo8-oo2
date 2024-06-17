c7810a73f99655006e03d2b87017a4cb13b7ca76
Cecilia <cecilia.anabel96@gmail.com>
2024-06-17 13:48:05 -0300

    Error 500

--- src/main/java/com/unla/grupo8_oo2/controllers/CarritoController.java
+++ src/main/java/com/unla/grupo8_oo2/controllers/CarritoController.java
@@ -1,6 +1,5 @@
 package com.unla.grupo8_oo2.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +7,6 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.unla.grupo8_oo2.services.ICarritoService;
-import com.unla.grupo8_oo2.services.IProductoService;
 import com.unla.grupo8_oo2.helpers.ViewRouteHelper;
 
 @Controller
@@ -17,7 +15,7 @@ public class CarritoController {
 
 	private ICarritoService carritoService;
 	
-	@Autowired
+	
 	public CarritoController(ICarritoService carritoService) {
 		this.carritoService = carritoService;
 	}
